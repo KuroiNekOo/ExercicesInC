719188fca147c5576c43c30978e458481efd34c9
Gato Preto <65861828+KuroiNekOo@users.noreply.github.com>
2022-06-11 22:13:35 +0200

    Update

---
+++ ft_dec_to_bin/ft_dec_to_bin.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_dec_to_bin.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgatopreto <maximegomes.dinis@outlook.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 23:50:38 by lgatopreto        #+#    #+#             */
+/*   Updated: 2022/06/11 22:11:08 by lgatopreto       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define ZERO '0'
+#define NINE '9'
+#define ASCII_OFFSET 48
+
+unsigned long	ft_atoi(char *nb)
+{
+	unsigned long	result;
+
+	result = 0;
+	if (*nb == '\0' || *nb == '-')
+		return (0);
+	if (*nb == '+')
+		++nb;
+	while (*nb >= ZERO && *nb <= NINE)
+	{
+		result = result * 10 + (*nb - ASCII_OFFSET);
+		ft_atoi(++nb);
+	}
+	return(result);
+}
+
+size_t	get_bin_size(unsigned long result)
+{
+	if (result != 0)
+	{
+		result /= 2;
+		return (1 + get_bin_size(result));
+	}
+	return (0);
+}
+
+void	calcul(char *res, unsigned long result, size_t size)
+{
+	while (size > 0)
+	{
+		--size;
+		res[size] = result % 2 + ASCII_OFFSET;
+		result /= 2;
+	}
+}
+
+char	*ft_dec_to_bin(char *nb)
+{
+	char	*res;
+
+	res = malloc(get_bin_size(ft_atoi(nb)) * sizeof(char));
+	calcul(res, ft_atoi(nb), get_bin_size(ft_atoi(nb)));
+	return (res);
+}
